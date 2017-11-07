d48390e9aa8dfd1efc74c45474c526485c2307bb
apariciojuan <32738434+apariciojuan@users.noreply.github.com>
2017-11-07 10:04:37 +0100

    Add files via upload
    server

---
+++ server/server.py
@@ -0,0 +1,34 @@
+import socketserver
+import threading
+import time
+
+#Los datos llegan en json, solo hay que transformarlos para cambiara al diccionario original
+class MiTcpHandler(socketserver.BaseRequestHandler):
+
+
+    def handle(self):
+        data= ""
+        while data != "salir":
+
+            print(self.request.recv(1024))
+            data = input("Semi_Shell>> ")
+            print(data)
+            self.request.send(data.encode('utf-8'))
+
+
+class ThreadServer(socketserver.ThreadingMixIn, socketserver.ForkingTCPServer):
+    pass
+
+def main():
+    host = ""
+    port= 33423
+    server = ThreadServer((host,port), MiTcpHandler)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.start()
+    print("corriendo")
+
+
+main()
+
+
+
