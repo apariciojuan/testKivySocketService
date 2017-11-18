54ccc5856380b9a443e024964d213391238c22f1
apariciojuan <32738434+apariciojuan@users.noreply.github.com>
2017-11-18 08:00:16 +0100

    extra
    extra

---
+++ MyBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package org.myproject.myapp;  
+import android.content.BroadcastReceiver;  
+import android.content.Intent;  
+import android.content.Context;  
+import org.renpy.android.PythonActivity;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {   
+   public void onReceive(Context context, Intent intent) {   
+         Intent ix = new Intent(context,PythonActivity.class);   
+         ix.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);    
+         context.startActivity(ix);    
+    }      
+} 
